7d5221e5c777496d41d988969a7d9fd1e5bcb930
Colin-Stark <collinscodes@gmail.com>
2024-05-17 09:48:09 -0400

    PRIVACY PROPERTY

--- week2/index.js
+++ week2/index.js
@@ -8,7 +8,7 @@ let blueprint = {
 
 blueprint.age = 40; // Change the age
 
-let result = blueprint.greet(); // Hello, my name is John and I am 30 years old.
+// let result = blueprint.greet(); // Hello, my name is John and I am 30 years old.
 
 
 // *************************** OR ********************************
@@ -17,7 +17,7 @@ let result = blueprint.greet(); // Hello, my name is John and I am 30 years old.
 /**
  * Creating a class with object literals
  */
-class Person {
+class Personel {
     name;
     age;
     constructor(name, age) {
@@ -30,11 +30,9 @@ class Person {
     }
 }
 
-const personal = new Person('John', 30);
+const personal = new Personel('John', 30);
 
-personal.greet(); // Hello, my name is John and I am 30 years old.
-
-
+// personal.greet(); // Hello, my name is John and I am 30 years old.
 
 // *************************** OR ********************************
 
@@ -45,11 +43,64 @@ class Personin {
         [this.name, this.age] = args;
     }
 
-    greet() {
-        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    greet(delay) {
+        setTimeout(() => {
+            console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+        }, delay);
     }
 }
 
 const persons = new Personin('bloke', 20);
 
-persons.greet(); 
+// persons.greet(1000);
+
+
+/**
+ * Working with private properties
+ */
+class Person {
+    name;
+    age;
+    email;
+    #sin;
+    address;
+    constructor(name, age, email, SIN, Addy) {
+        this.name = name;
+        this.age = age;
+        this.email = email;
+        this.#sin = SIN;
+        this.address = Addy;
+    }
+
+    get sin() {
+        return this.#sin = 'xxx-xx-xxxx';
+    }
+
+    set sin(value) {
+        this.#sin = value;
+    }
+
+    display() {
+        console.log(
+            `
+            Name: ${this.name}
+            Age: ${this.age}
+            Email: ${this.email}
+            Address: ${this.address}
+            `
+        );
+    }
+}
+
+
+let newPerson = new Person('John', 30, 'john@test.com', 1234567890, '12 James Street, Toronto, ON, M5C 2H5');
+
+newPerson.display();
+console.log(newPerson.sin);
+console.log(newPerson.name);
+
+
+
+
+
+
